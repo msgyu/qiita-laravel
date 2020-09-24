e699320c0bad51f03cfae71eef75198ab47b64c6
nousi <nousidayo@gmail.com>
2020-09-25 05:11:26 +0900

    edit header.scss

--- resources/views/parts/header.blade.php
+++ resources/views/parts/header.blade.php
@@ -68,7 +68,7 @@
               <span class="caret"></span>
             </a>
 
-            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
               <a class="dropdown-item" href="#">マイページ</a>
               <div class="dropdown-divider"></div>
               <a class="dropdown-item" href="#">下書き一覧</a>
@@ -79,7 +79,7 @@
               <div class="dropdown-divider"></div>
               <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
                                                          document.getElementById('logout-form').submit();">
-                {{ __('Logout') }}
+                {{ __('ログアウト') }}
               </a>
 
               <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
