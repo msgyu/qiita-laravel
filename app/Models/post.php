0397d8ac1d9cbeeabe614687ac5607913cf81ce3
nousi <nousidayo@gmail.com>
2020-09-25 06:23:00 +0900

    Add relation of post

--- app/Models/post.php
+++ app/Models/post.php
@@ -9,4 +9,13 @@ class post extends Model
     protected $fillable = [
         'title', 'body'
     ];
+
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
+    public function tags()
+    {
+        return $this->belongsToMany('App\Models\Tag', 'post_tags');
+    }
 }
