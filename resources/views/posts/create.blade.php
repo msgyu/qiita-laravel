4f472db4c3f1702ffc1076d3707a04cda3a78f63
nousi <nousidayo@gmail.com>
2020-09-25 09:36:03 +0900

    Add tag to form of create.blade.php

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -4,12 +4,21 @@
 <form class="post-page-wrapper" action="{{ route('posts.store') }}" method="post">
   @csrf
   <input type="text" class="form-control m-1" id="title-input" placeholder="タイトル" name="title">
-  <input type="text" class="form-control m-1" placeholder="プログラミング技術に関するタグを入力" name="tags">
+  <ul class="tag-wrapper ">
+    <li class="tag-content">
+      <span class="tag-label">タグ</span>
+      <a class="tag-close">×</a>
+      <input class="tag-hidden-field" value="タグ" type="hidden" />
+    </li>
+    <li class="tag-new">
+      <input id="tag-input" name="tags[]['name']" class="tag-input ui-autocomplete-input" placeholder="プログラミング技術に関するタグを入力" autocomplete="off" type="text" />
+    </li>
+  </ul>
   <div class="row">
-    <div class="col-6 m-1">
-      <textarea name="body" id="markdown_editor_textarea" cols="30" rows="10" class="form-control"></textarea>
+    <div class="col-6">
+      <textarea name="body" id="markdown_editor_textarea" placeholder="プログラミング知識をmarkdonw記法で書いて共有" cols="30" rows="10" class="form-control"></textarea>
     </div>
-    <div class="col-6 m-1">
+    <div class="col-6">
       <div id="markdown_preview"></div>
     </div>
   </div>
