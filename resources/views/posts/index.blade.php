58110053c399b08ac2b6b35ce0bbddbe438af467
nousi <nousidayo@gmail.com>
2020-09-25 20:19:04 +0900

    change articles to posts

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -2,14 +2,14 @@
 
 @section('content')
 <div class="top-wrapper">
-  <div class="articles-wrapper col-md-6">
+  <div class="posts-wrapper col-md-6">
     @foreach ($posts as $post)
-    <div class="article-box">
-      <div class="article-box-left"></div>
-      <div class="article-box-right">
-        <a class="article-title" href="/drafts/{{$article->id}}">{{ $post->title }}</a>
-        <div class="article-details">
-          <div class="article-date">{{ $post->created_at }}</div>
+    <div class="post-box">
+      <div class="post-box_left"></div>
+      <div class="post-box_right">
+        <a class="post-title" href="{{ route('posts.show', $post)}}">{{ $post->title }}</a>
+        <div class="post-details">
+          <div class="post-details_date">{{ $post->created_at }}</div>
         </div>
       </div>
     </div>
