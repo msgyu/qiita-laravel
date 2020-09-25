bd6685fb967d1e03f4cc0cf8bc919751a27a9c7e
nousi <nousidayo@gmail.com>
2020-09-25 14:55:15 +0900

    Add fillable

--- app/Models/post.php
+++ app/Models/post.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class post extends Model
 {
     protected $fillable = [
-        'title', 'body'
+        'title', 'body', 'user_id'
     ];
 
     public function user()
