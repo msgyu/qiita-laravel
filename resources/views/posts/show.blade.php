46e2cf927fdf2d22be7d42acc2b8af5b9d99dad8
nousi <nousidayo@gmail.com>
2020-09-25 15:46:40 +0900

    Update show.blade.php

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,5 +1,18 @@
 @extends('layouts.app')
 
 @section('content')
-
+<div class="post-page-wrapper">
+  <div class="post-wrapper">
+    <div class="post-header">
+      <div class="date">{{ $post->created_at }}</div>
+    </div>
+    <div class="post-title"> {{ $post->title }}</div>
+    @if($post->tags())
+    @foreach ($post->tags() as $tag)
+    <div class="post-tag">{{$tag}}</div>
+    @endforeach
+    @endif
+    <div class="post-body">{{$post->body}}</div>
+  </div>
+</div>
 @endsection
