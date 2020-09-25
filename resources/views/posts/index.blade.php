f5a0dc1f906ceed78582d0e0211509ec062f7258
nousi <nousidayo@gmail.com>
2020-09-25 20:05:25 +0900

    Update posts / index.blade.php

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,5 +1,19 @@
 @extends('layouts.app')
 
 @section('content')
-<div>これが表示されたら成功</div>
+<div class="top-wrapper">
+  <div class="articles-wrapper col-md-6">
+    @foreach ($posts as $post)
+    <div class="article-box">
+      <div class="article-box-left"></div>
+      <div class="article-box-right">
+        <a class="article-title" href="/drafts/{{$article->id}}">{{ $post->title }}</a>
+        <div class="article-details">
+          <div class="article-date">{{ $post->created_at }}</div>
+        </div>
+      </div>
+    </div>
+    @endforeach
+  </div>
+</div>
 @endsection
