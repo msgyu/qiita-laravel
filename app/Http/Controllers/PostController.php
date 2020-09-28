c32463f4f213ba1331f227cd81a09bf87345e4a4
nousi <nousidayo@gmail.com>
2020-09-28 23:59:59 +0900

    Add $request
    for search form

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -14,8 +14,10 @@ class PostController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
+        $keyward = $request->name('search');
+
         if (Auth::check()) {
             return view('posts.index');
         } else {
