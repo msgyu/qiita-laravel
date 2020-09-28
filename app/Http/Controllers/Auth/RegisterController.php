30cf83f2903a72b0292554b79f73dfc997e42fa9
nousi <nousidayo@gmail.com>
2020-09-28 23:14:48 +0900

    Add use Intervention\Image\Facades\Image;

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -7,6 +7,7 @@ use App\User;
 use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
+use Intervention\Image\Facades\Image;
 
 class RegisterController extends Controller
 {
@@ -52,6 +53,7 @@ class RegisterController extends Controller
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
+            'icon' => ['file', 'image', 'mimes:jpeg,png,jpg,gif', 'max:2000'],
         ]);
     }
 
