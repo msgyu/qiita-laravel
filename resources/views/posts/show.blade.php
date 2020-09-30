e283778d18f904aa356b21d8269cec28cc6951d8
nousi <nousidayo@gmail.com>
2020-09-30 14:25:19 +0900

    edit show.blade.php

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -2,6 +2,18 @@
 
 @section('content')
 <div class="post-page-wrapper">
+  <div class="show-side">
+    <div class="show-side_wrapper">
+      <div class="like">
+        <a href="">3</a>
+        <button class="like_btn">
+          <i class="far fa-heart"></i>
+        </button>
+      </div>
+    </div>
+    aaa
+  </div>
+
   <div class="post-wrapper">
     <div class="post-header">
       <ul class="post-header_info">
@@ -20,5 +32,6 @@
     @endif
     <div class="post-body">{{$post->body}}</div>
   </div>
+
 </div>
 @endsection
