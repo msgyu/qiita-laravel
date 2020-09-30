e893d22232fd8a160664ebe0154e312d312e735e
nousi <nousidayo@gmail.com>
2020-09-30 14:30:13 +0900

    Add  html of index.blade.php to top.blade.php

--- resources/views/posts/top.blade.php
+++ resources/views/posts/top.blade.php
@@ -33,4 +33,19 @@
 </div>
 @else
 @endif
+<div class="top-wrapper">
+  <div class="posts-wrapper col-md-6">
+    @foreach ($posts as $post)
+    <div class="post-box">
+      <img class="post-box_left" src="{{ asset('./img/sample-user.png') }}" alt="ロゴ">
+      <div class="post-box_right">
+        <a class="post-title" href="{{ route('posts.show', $post->id)}}">{{ $post->title }}</a>
+        <div class="post-details">
+          <div class="post-details_date">{{ $post->created_at }}</div>
+        </div>
+      </div>
+    </div>
+    @endforeach
+  </div>
+</div>
 @endsection
