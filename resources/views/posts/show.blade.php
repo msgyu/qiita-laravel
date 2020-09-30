dd1d03c8f35fa949bdffe453cb817793b49d5ae4
nousi <nousidayo@gmail.com>
2020-09-30 15:35:41 +0900

    Add edit button

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -4,10 +4,36 @@
 <div class="post-page-wrapper">
   <div class="post-wrapper">
     <div class="post-header">
-      <ul class="post-header_info">
+      <ul class="post-header_ul-left">
         <li><img class="user-icon" width="32px" height="32px" src="{{ asset('./img/sample-user.png') }}" alt="ロゴ"></li>
         <li class="user-name">{{ $post->user->name }}</li>
         <li class="date">{{ $post->created_at }}</li>
+      </ul>
+      <ul class="post-header_ul-right">
+        @if (Auth::check())
+        @if($post->user_id === Auth::user()->id)
+        <li>
+          <a href="" class="edit-pass">
+            <span>
+              <i class="fas fa-pencil-alt"></i>
+              編集する
+            </span>
+          </a>
+        </li>
+        <li>
+          <div class="dropdown show">
+            <a class="dropdown-toggle setting" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              <span><i class="fas fa-cog"></i></span>
+            </a>
+            <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+              <a class="dropdown-item" href="#">Action</a>
+              <a class="dropdown-item" href="#">Another action</a>
+              <a class="dropdown-item" href="#">Something else here</a>
+            </div>
+          </div>
+        </li>
+        @endif
+        @endif
       </ul>
     </div>
     <div class="post-title"> {{ $post->title }}</div>
