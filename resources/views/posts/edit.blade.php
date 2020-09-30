ee78da03c0464f69290774e465392eb82f21fd50
nousi <nousidayo@gmail.com>
2020-09-30 16:56:07 +0900

    Add initial value of tags

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -5,6 +5,17 @@
   @csrf
   <input type="text" class="form-control" id="title-input" value="{{ $post->title }}" placeholder="タイトル" name="title">
   <ul class="tag-wrapper ">
+    @foreach($post->tags as $tag)
+    <li class="tag-content">
+      <span class="tag-label">
+        {{$tag->name}}
+      </span>
+      <a class="text-icon">
+        ×
+      </a>
+      <input class="tag-hidden-field" name="tags[]" value="{{$tag->name}}" type="hidden">
+    </li>
+    @endforeach
     <li class="tag-new">
       <input id="tag-input" name="tags[]" class="tag-input" placeholder="プログラミング技術に関するタグを入力" type="text" />
     </li>
