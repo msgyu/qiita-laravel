f5a05d216caaeec9454a5bdc4026e612b085b8db
nousi <nousidayo@gmail.com>
2020-10-05 14:34:27 +0900

    Add migration  of likes

--- database/migrations/2020_10_05_143025_create_likes_table.php
+++ database/migrations/2020_10_05_143025_create_likes_table.php
@@ -15,6 +15,10 @@ class CreateLikesTable extends Migration
     {
         Schema::create('likes', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->unsignedBigInteger('post_id');
+            $table->foreign('post_id')->references('id')->on('posts');
             $table->timestamps();
         });
     }
