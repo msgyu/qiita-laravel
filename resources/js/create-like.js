47990d92ca6189e89c8b0d03a2a80501a6eb29eb
nousi <nousidayo@gmail.com>
2020-10-06 16:01:16 +0900

    Writing Ajax to create like

--- resources/js/create-like.js
+++ resources/js/create-like.js
@@ -1,15 +1,32 @@
 $(function() {
     $(".like_btn").click(function() {
-        var post_id = $(".like_btn").data("post_id");
+        var post_id = $(".like_btn").attr("post_id");
+        var like_exist = $(".like_btn").attr("like_exist");
+        click_button = $(this);
+        console.log(post_id);
         $.ajax({
-            url: "/likes",
+            headers: {
+                "X-CSRF-TOKEN": $('meta[name="csrf-token"]').attr("content")
+            },
+            url: "/like_product",
             type: "POST",
-            dataType: "json",
-            data: { id: post_id }
+            data: { post_id: post_id, like_exist: like_exist }
         })
-            .done(function(comments) {})
-            .fail(function() {
-                alert("通信に失敗しました");
+            .done(function(like_exist) {
+                console.log("成功");
+                if (like_exist == 0) {
+                    click_button.attr("like_exist", "1");
+                    click_button.css({ color: "#fff", background: "#55c500" });
+                    // click_button.children().attr("class", "fas fa-heart");
+                }
+                if (like_exist == 1) {
+                    click_button.attr("like_exist", "0");
+                    click_button.css({ color: "#55c500", background: "#fff" });
+                }
+            })
+            .fail(function(data) {
+                alert("いいね処理失敗");
+                alert(JSON.stringify(data));
             });
     });
 });
