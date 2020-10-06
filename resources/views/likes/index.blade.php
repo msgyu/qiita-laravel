8b99c1a9eb4051e1fae71c823bd6fb3aba9be206
nousi <nousidayo@gmail.com>
2020-10-07 02:18:01 +0900

    Add html to index.blade.php of likes
    Copy index.blade.php of posts

---
+++ resources/views/likes/index.blade.php
@@ -0,0 +1,41 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="top-wrapper">
+  <div class="posts-wrapper col-md-6">
+    <div class="posts-nav">
+      <ul class="posts-nav_ul">
+        <li>
+          <a href="">
+            すべて
+            <span class="badge">{{ count($posts)}}</span>
+          </a>
+        </li>
+        @auth
+        <li class="active">
+          <a href="">
+            LGTM済み
+            <span class="badge">{{ count(Auth::user()->likes)}}</span>
+          </a>
+        </li>
+        @endauth
+      </ul>
+    </div>
+    @if(count($posts) !== 0)
+    @foreach ($posts as $post)
+    <div class="post-box">
+      <img class="post-box_left" src="{{ asset('./img/sample-user.png') }}" alt="ロゴ">
+      <div class="post-box_right">
+        <a class="post-title" href="{{ route('posts.show', $post->id)}}">{{ $post->title }}</a>
+        <div class="post-details">
+          <div class="post-details_date">{{ $post->created_at }}</div>
+        </div>
+      </div>
+    </div>
+    @endforeach
+    @else
+    <p>「{{ $keyword }}」に一致する記事は見つかりませんでした。</p>
+    @endif
+  </div>
+</div>
+@endsection
