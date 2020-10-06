84f5f041c56fe1d59c02f0a56f129fe5e3b435b5
nousi <nousidayo@gmail.com>
2020-10-06 15:58:54 +0900

    Add post and user associations to the like model

--- app/Models/like.php
+++ app/Models/like.php
@@ -9,4 +9,14 @@ class like extends Model
     protected $fillable = [
         'user_id', 'post_id'
     ];
+
+    public function post()
+    {
+        return $this->belongsTo('App\Models\Post');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
 }
