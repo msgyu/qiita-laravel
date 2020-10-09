f0c6adb526b41d4c6d135fbee238f507aa6fb8d8
nousi <nousidayo@gmail.com>
2020-10-10 02:33:57 +0900

    Add if branch in posts_index.blade.php

--- resources/views/parts/posts_index.blade.php
+++ resources/views/parts/posts_index.blade.php
@@ -2,7 +2,11 @@
 <div class="post-box">
   <img class="post-box_left" src="{{ asset('./img/sample-user.png') }}" alt="ロゴ">
   <div class="post-box_right">
+    @if (Route::currentRouteName() === 'likes.index')
+    <a class="post-title" href="{{ route('posts.show', $post->post_id)}}">{{ $post->title }}</a>
+    @else
     <a class="post-title" href="{{ route('posts.show', $post->id)}}">{{ $post->title }}</a>
+    @endif
     <div class="post-details">
       <div class="post-details_date">{{ $post->created_at }}</div>
     </div>
