b470b823f192904cef6ff1363249e907b95443fb
nousi <nousidayo@gmail.com>
2020-10-11 03:14:29 +0900

    Add HTML & CSS of date & like in details-search

--- resources/views/likes/index.blade.php
+++ resources/views/likes/index.blade.php
@@ -7,14 +7,52 @@
       <div class="detailed-search_form_head">記事の条件</div>
       <div class="detailed-search_form_body">
         <div class="search-conditions date-terms">
-          <span class="search-conditions_title">日付</span>
+          <div>
+            <span class="search-conditions_title">日付</span>
+          </div>
           <div class="search-conditions_radio">
-            <input type="radio" name="date-terms" value="day">1日
+            <input type="radio" name="date-terms" value="day" class="">1日
+          </div>
+          <div class="search-conditions_radio">
             <input type="radio" name="date-terms" value="week">1週間
+          </div>
+          <div class="search-conditions_radio">
             <input type="radio" name="date-terms" value="month">月間
+          </div>
+          <div class="search-conditions_radio">
             <input type="radio" name="date-terms" value="period">期間指定
           </div>
+          <div class="search-conditions_radio">
+            <span>
+              <label for="">
+                開始:<input type="date" class="input-small" value="2020-01-11" placeholder="開始">
+              </label>
+            </span>
+            <span>
+              <label for="">
+                終了:<input type="date" class="input-small" value="2020-01-11" placeholder="終了">
+              </label>
+            </span>
+          </div>
         </div>
+        <div class="search-conditions lgtm-terms">
+          <div>
+            <span class="search-conditions_title lgtm-terms_title">LGTM</span>
+          </div>
+          <div class="search-conditions_radio">
+            <span>
+              <label for="">
+                最低:<input type="number" class="input-small" value="2020-01-11" placeholder="開始">
+              </label>
+            </span>
+            <span>
+              <label for="">
+                最高:<input type="number" class="input-small" value="2020-01-11" placeholder="終了">
+              </label>
+            </span>
+          </div>
+        </div>
+
       </div>
     </form>
   </div>
