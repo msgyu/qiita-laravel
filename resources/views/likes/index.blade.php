bafef51e5db499c7b6065b948f0c5c5f8e99aa45
nousi <nousidayo@gmail.com>
2020-10-10 23:50:21 +0900

    Add details-search html

--- resources/views/likes/index.blade.php
+++ resources/views/likes/index.blade.php
@@ -2,7 +2,23 @@
 
 @section('content')
 <div class="top-wrapper">
-  <div class="posts-wrapper col-md-6">
+  <div class="detailed-search col-md-3">
+    <form action="" class="detailed-search_form">
+      <div class="detailed-search_form_head">記事の条件</div>
+      <div class="detailed-search_form_body">
+        <div class="search-conditions date-terms">
+          <span class="search-conditions_title">日付</span>
+          <div class="search-conditions_radio">
+            <input type="radio" name="date-terms" value="day">1日
+            <input type="radio" name="date-terms" value="week">1週間
+            <input type="radio" name="date-terms" value="month">月間
+            <input type="radio" name="date-terms" value="period">期間指定
+          </div>
+        </div>
+      </div>
+    </form>
+  </div>
+  <div class="posts-wrapper col-md-5">
     <div class="posts-nav">
       <ul class="posts-nav_ul">
         <li>
