7b888f19ce818c50eb25160ca94e8cccd233c184
nousi <nousidayo@gmail.com>
2020-10-11 12:16:32 +0900

    Delete console.log

--- resources/js/create-like.js
+++ resources/js/create-like.js
@@ -13,11 +13,9 @@ $(function() {
             data: { post_id: post_id, like_exist: like_exist }
         })
             .done(function(like_exist) {
-                console.log("成功");
                 if (like_exist == 0) {
                     click_button.attr("like_exist", "1");
                     click_button.css({ color: "#fff", background: "#55c500" });
-                    // click_button.children().attr("class", "fas fa-heart");
                 }
                 if (like_exist == 1) {
                     click_button.attr("like_exist", "0");
@@ -25,8 +23,7 @@ $(function() {
                 }
             })
             .fail(function(data) {
-                alert("いいね処理失敗");
-                alert(JSON.stringify(data));
+                alert("いいね処理に失敗しました");
             });
     });
 });
