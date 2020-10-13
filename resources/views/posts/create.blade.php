b2afa38c10671b92f038bc94af9ad43010440e1d
nousi <nousidayo@gmail.com>
2020-10-14 04:00:52 +0900

    Delete comment out

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -26,27 +26,4 @@
     </button>
   </div>
 </form>
-<!-- <form class="post-page-wrapper" action="{{ route('posts.store') }}" method="post">
-  @csrf
-  <input type="text" class="form-control" id="title-input" placeholder="タイトル" name="title">
-  <ul class="tag-wrapper ">
-    <li class="tag-new">
-      <input id="tag-input" name="tags[]" class="tag-input" placeholder="プログラミング技術に関するタグを入力" type="text" />
-    </li>
-  </ul>
-  <div class="row">
-    <div class="markdown">
-      <textarea name="body" id="markdown_editor_textarea" placeholder="プログラミング知識をmarkdonw記法で書いて共有" cols="30" rows="10"></textarea>
-    </div>
-    <div class="markdown">
-      <div id="markdown_preview"></div>
-    </div>
-  </div>
-  <div class="post-page-footer">
-    <button type='submit' class="post-button" tabindex="41">
-      <i class="fa fa-upload"></i>
-      Oiita に投稿
-    </button>
-  </div>
-</form> -->
 @endsection
