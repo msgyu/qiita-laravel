eb6ebd533492c7ed6b006763f470583ab3017d56
nousi <nousidayo@gmail.com>
2020-10-13 19:53:07 +0900

    Add preview css

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -11,7 +11,12 @@
       </li>
     </ul>
     <div class="markdown-wrapper">
-
+      <div class="markdown">
+        <textarea name="body" id="markdown_editor_textarea" placeholder="プログラミング知識をmarkdonw記法で書いて共有" cols="30" rows="10"></textarea>
+      </div>
+      <div class="markdown">
+        <div id="markdown_preview"></div>
+      </div>
     </div>
   </div>
   <div class="post-create-footer">
