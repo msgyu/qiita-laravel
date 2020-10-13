7c2b7721baff2ebf2cf43df41c5129d400c3014d
nousi <nousidayo@gmail.com>
2020-10-13 18:43:07 +0900

    Change class .row to .markdown-wrapper
    Change the class name to not use bootstrap

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -2,8 +2,17 @@
 
 @section('content')
 <form class="post-page-wrapper" action="{{ route('posts.store') }}" method="post">
+  @csrf
   <div class="post-create_wrapper">
-    aaaa
+    <input type="text" class="form-control" id="title-input" placeholder="タイトル" name="title">
+    <ul class="tag-wrapper">
+      <li class="tag-new">
+        <input id="tag-input" name="tags[]" class="tag-input" placeholder="プログラミング技術に関するタグを入力" type="text" />
+      </li>
+    </ul>
+    <div class="markdown-wrapper">
+
+    </div>
   </div>
   <div class="post-create-footer">
     <button type='submit' class="post-button" tabindex="41">
