ce4396ab2f90a3200507f3b3dfec55da8af6e90f
nousi <nousidayo@gmail.com>
2020-10-22 02:28:55 +0900

    tagのinputにおいて、.tagsを親階層に変更

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -5,11 +5,12 @@
   @csrf
   <div class="post-create_wrapper">
     <input type="text" class="form-control" id="title-input" placeholder="タイトル" name="title">
-    <ul class="tag-wrapper">
+    <div class="tags">
+      <ul class="tag-wrapper"></ul>
       <div class="tag-new">
         <input id="tag-input" name="tags[]" class="tag-input" placeholder="プログラミング技術に関するタグを入力" type="text" />
       </div>
-    </ul>
+    </div>
     <div class="markdown-wrapper">
       <div class="markdown">
         <textarea name="body" id="markdown_editor_textarea" placeholder="プログラミング知識をmarkdonw記法で書いて共有" cols="30" rows="10"></textarea>
