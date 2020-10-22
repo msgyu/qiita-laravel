e76fd65ef229e055ff5d34124279ba223f27fbbd
nousi <nousidayo@gmail.com>
2020-10-22 22:57:21 +0900

    Update edit.blade.php
    create.blade.phpに合わせて更新。

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -6,20 +6,23 @@
   @method('PUT')
   <div class="post-create_wrapper">
     <input type="text" class="form-control" id="title-input" value="{{ $post->title }}" placeholder="タイトル" name="title">
-    <ul class="tag-wrapper ">
-      @foreach($post->tags as $tag)
-      <li class="tag-content">
-        <span class="tag-label">
-          {{$tag->name}}
-          <a class="text-icon">
-            ×
-          </a>
-        </span>
-        <input class="tag-hidden-field" name="tags[]" value="{{$tag->name}}" type="hidden">
-      </li>
-      @endforeach
-      <input id="tag-input" name="tags[]" class="tag-input" placeholder="プログラミング技術に関するタグを入力" type="text" />
-    </ul>
+    <div class="tags">
+      <ul class="tags-wrapper">
+        @foreach($post->tags as $tag)
+        <li class="tag-content">
+          <span class="tag-label">
+            {{$tag->name}}
+            <a class="text-icon">
+              ×
+            </a>
+          </span>
+          <input class="tag-hidden-field" name="tags[]" value="{{$tag->name}}" type="hidden">
+        </li>
+        @endforeach
+
+      </ul>
+      <input id="tag-input" name="tags[]" placeholder="プログラミング技術に関するタグを入力" type="text" />
+    </div>
     <div class="markdown-wrapper">
       <div class="markdown">
         <textarea name="body" id="markdown_editor_textarea" placeholder="プログラミング知識をmarkdonw記法で書いて共有" cols="30" rows="10">{{ $post->body }}</textarea>
