0cbff81092b5a58befe84473ec84f14016b59b5c
nousi <nousidayo@gmail.com>
2020-10-23 23:47:03 +0900

    use post in like factory

--- database/factories/LikeFactory.php
+++ database/factories/LikeFactory.php
@@ -3,9 +3,12 @@
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
 use App\Models\like;
+use App\Models\post;
 use Faker\Generator as Faker;
 
 $factory->define(like::class, function (Faker $faker) {
+    $postIDs  = App\Models\post::pluck('id')->all();
+    $userIDs  = App\User::pluck('id')->all();
     return [
         //
     ];
