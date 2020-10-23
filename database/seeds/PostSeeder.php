8206c6dd5476e2605100f989abe080dabe55dd25
nousi <nousidayo@gmail.com>
2020-10-24 04:51:18 +0900

    PostSeederの修正
    多対多のため、attachでリレーションさせている

--- database/seeds/PostSeeder.php
+++ database/seeds/PostSeeder.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Seeder;
 use App\Models\Post;
+use App\Models\Tag;
 
 class PostSeeder extends Seeder
 {
@@ -12,6 +13,13 @@ class PostSeeder extends Seeder
      */
     public function run()
     {
-        factory(Post::class, 200)->create();
+        $tags = App\Models\Tag::all();
+        factory(Post::class, 200)
+            ->create()
+            ->each(function ($post) use ($tags) {
+                $post->tags()->attach(
+                    $tags->random(rand(1, 3))->pluck('id')->toArray()
+                );
+            });
     }
 }
