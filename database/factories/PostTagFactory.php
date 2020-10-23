b11f08c4503b12fc7f84df8cdc2e804931ef5bad
nousi <nousidayo@gmail.com>
2020-10-23 23:49:55 +0900

    define $postIDs in post_tag factory

--- database/factories/PostTagFactory.php
+++ database/factories/PostTagFactory.php
@@ -3,9 +3,12 @@
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
 use App\post_tag;
+use App\Models\Post;
 use Faker\Generator as Faker;
 
 $factory->define(post_tag::class, function (Faker $faker) {
+    $postIDs  = App\Models\Post::pluck('id')->all();
+    $tagIDs  = App\Models\Tag::pluck('id')->all();
     return [
         //
     ];
