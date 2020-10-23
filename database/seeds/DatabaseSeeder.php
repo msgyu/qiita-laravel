cc9465fc6a7c0aed654c109794f48ba956d020df
nousi <nousidayo@gmail.com>
2020-10-23 23:55:41 +0900

    Add post tag seeder in database seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,5 +14,6 @@ class DatabaseSeeder extends Seeder
         $this->call(UsersTableSeeder::class);
         $this->call(PostSeeder::class);
         $this->call(TagSeeder::class);
+        $this->call(PostTagSeeder::class);
     }
 }
