028b2b3b957f300832de14f5c5b8a28bf8da0a2b
nousi <nousidayo@gmail.com>
2020-10-24 06:36:06 +0900

    PostSeederにおいて、繰り返し処理でLikeを作成
    for文を利用して、Likeを複製している。

--- database/factories/LikeFactory.php
+++ database/factories/LikeFactory.php
@@ -3,11 +3,10 @@
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
 use App\Models\like;
-use App\Models\post;
 use Faker\Generator as Faker;
 
 $factory->define(like::class, function (Faker $faker) {
-    $postIDs  = App\Models\post::pluck('id')->all();
+    $postIDs  = App\Models\Post::pluck('id')->all();
     $userIDs  = App\User::pluck('id')->all();
     return [
         'post_id' => $faker->randomElement($postIDs),
--- database/seeds/PostSeeder.php
+++ database/seeds/PostSeeder.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Seeder;
 use App\Models\Post;
 use App\Models\Tag;
+use App\Models\Like;
 
 class PostSeeder extends Seeder
 {
@@ -14,12 +15,18 @@ class PostSeeder extends Seeder
     public function run()
     {
         $tags = App\Models\Tag::all();
+        $users = App\User::pluck('id')->all();
         factory(Post::class, 200)
             ->create()
-            ->each(function ($post) use ($tags) {
+            ->each(function ($post) use ($tags, $users) {
                 $post->tags()->attach(
                     $tags->random(rand(1, 3))->pluck('id')->toArray()
                 );
+                for ($count = 0; $count < rand(10, 200); $count++) {
+                    $post->likes()->create([
+                        'user_id' => $users[array_rand($users)],
+                    ]);
+                }
             });
     }
 }
