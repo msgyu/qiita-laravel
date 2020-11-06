c0912796f1c8b435a8eef59f50b843ef3d6121ea
nousi <nousidayo@gmail.com>
2020-11-06 19:12:54 +0900

    RouteNameによって、.post-tagのformのactionが変化する
    タグボタンを押せば、そのタグに関する記事を検索できるが、
    posts.indexのみ対応していた。それ以外も対応することでいいねした記事や自分の記事一覧でも可能となった

--- resources/views/parts/posts_index.blade.php
+++ resources/views/parts/posts_index.blade.php
@@ -12,9 +12,15 @@
       <div class="post-tags" style="display:inline-flex">
         @foreach ($post->tags as $tag)
         <div>
-          <form action="{{ route('posts.index')}}" method="GET">
-            <button class="post-tag" name="tag_btn" value="{{ optional($tag)->name }}">{{ optional($tag)->name }}</button>
-          </form>
+          @if (Route::currentRouteName() === 'likes.index')
+          <form action="{{ route('likes.index')}}" method="GET">
+            @elseif (Route::currentRouteName() === 'my_posts')
+            <form action="{{ route('my_posts')}}" method="GET">
+              @else
+              <form action="{{ route('posts.index')}}" method="GET">
+                @endif
+                <button class="post-tag" name="tag_btn" value="{{ optional($tag)->name }}">{{ optional($tag)->name }}</button>
+              </form>
         </div>
         @endforeach
       </div>
