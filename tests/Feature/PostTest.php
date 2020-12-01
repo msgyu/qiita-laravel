a46e1dde64461ba2c7a9e52178f4909251a11b04
nousi <nousidayo@gmail.com>
2020-12-02 00:02:02 +0900

    ポスト一覧のテストを実行
    testPostsIndex

--- tests/Feature/PostTest.php
+++ tests/Feature/PostTest.php
@@ -19,4 +19,11 @@ class PostTest extends TestCase
 
         $response->assertStatus(200);
     }
+
+    public function testPostsIndex()
+    {
+        $response = $this->get('/posts');
+
+        $response->assertStatus(200);
+    }
 }
